79eea59fa9a3517a5f6b448bef936e5c79bda82b
Charlenni <web@weltz-online.de>
2025-04-02 13:27:36 +0200

    Add new project for commen things for renderer

---
+++ source/renderers/VexTile.Renderer.Common/Interfaces/IPaintFactory.cs
@@ -0,0 +1,10 @@
+﻿using SkiaSharp;
+using VexTile.Common.Interfaces;
+using VexTile.Common.Primitives;
+
+namespace VexTile.Renderer.Common.Interfaces;
+
+public interface IPaintFactory
+{
+    IEnumerable<SKPaint> CreateOrUpdatePaint(ITileStyle style, EvaluationContext context);
+}
