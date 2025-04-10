621cbdc72782096374f7bf52b1ccb0bad21f3c02
Charlenni <web@weltz-online.de>
2025-04-10 21:52:16 +0200

    Add link to files in Converter tests and correct the tile numbers to Google system

--- tests/VexTile.Converters.Tests/MapboxTileConverter.Tests.cs
+++ tests/VexTile.Converters.Tests/MapboxTileConverter.Tests.cs
@@ -6,7 +6,7 @@ namespace VexTile.Readers.Tests;
 
 public class MapboxConverterTests
 {
-    readonly string _path = "..\\..\\..\\..\\..\\tests\\files\\zurich.mbtiles";
+    readonly string _path = "files\\zurich.mbtiles";
 
     [Fact]
     public async Task VectorTileConverterTest()
@@ -17,15 +17,15 @@ public class MapboxConverterTests
 
         var tileConverter = new MapboxTileConverter(dataSource);
 
-        var vectorTile = await tileConverter.Convert(new NetTopologySuite.IO.VectorTiles.Tiles.Tile(8580, 10645, 14));
+        var vectorTile = await tileConverter.Convert(new NetTopologySuite.IO.VectorTiles.Tiles.Tile(8580, 5738, 14));
 
         Assert.NotNull(vectorTile);
-        Assert.True(vectorTile.TileId == 263894745);
+        Assert.True(vectorTile.TileId == 183498457);
         Assert.True(vectorTile.IsEmpty == false);
         Assert.True(vectorTile.Layers.Count == 12);
         Assert.True(vectorTile.Layers[0].Name == "water");
         Assert.True(vectorTile.Layers[0].Features.Count == 9);
-        Assert.True(vectorTile.Layers[0].Features[0].Attributes.Count == 1);
+        Assert.True(vectorTile.Layers[0].Features[0].Attributes.Count == 2);
         Assert.True(vectorTile.Layers[0].Features[0].Attributes.GetNames()[0] == "class");
         Assert.True(vectorTile.Layers[0].Features[0].Attributes.GetValues()[0].ToString() == "river");
     }
