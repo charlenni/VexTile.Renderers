45e7642ffacc9cda296b4a100c384c5e346a910c
Charlenni <web@weltz-online.de>
2025-04-10 14:17:27 +0200

    Add new tests to create SVGs for tiles

--- tests/VexTile.Styles.MapboxTests/Mapbox.Render.Tests.cs
+++ tests/VexTile.Styles.MapboxTests/Mapbox.Render.Tests.cs
@@ -1,8 +1,9 @@
-﻿using VexTile.Style.Mapbox;
-using VexTile.Renderer.Bitmap;
-using Xunit;
-using VexTile.Renderer.Mapbox;
+﻿using SkiaSharp;
 using VexTile.Common.Interfaces;
+using VexTile.Renderer.Common.Extensions;
+using VexTile.Renderer.Mapbox;
+using VexTile.Style.Mapbox;
+using Xunit;
 
 namespace VexTile.Styles.MapboxTests;
 
@@ -22,36 +23,43 @@ public class MapboxRenderTests
 
     [Theory]
     [InlineData(0, 0, 0)]
-    [InlineData(1, 1, 1)]
+    [InlineData(1, 0, 1)]
     [InlineData(2, 1, 2)]
     [InlineData(33, 22, 6)]
     [InlineData(1072, 717, 11)]
     [InlineData(8580, 5738, 14)]
     [InlineData(8580, 5733, 14)]
     [InlineData(8581, 5733, 14)]
-    public async Task RenderTileTest1(int x, int y, int z)
+    public async Task RenderTilePngTest(int x, int y, int z)
     {
-        var data = await _renderer.Render(new NetTopologySuite.IO.VectorTiles.Tiles.Tile(x, y, z));
+        var picture = await _renderer.Render(new NetTopologySuite.IO.VectorTiles.Tiles.Tile(x, y, z));
 
-        Assert.True(data != null);
-        Assert.True(!data.IsEmpty);
-        //Assert.True(data.Size == 244799);
+        Assert.True(picture != null);
 
         using (var stream = new FileStream(Path.Combine(_path, $"{x}-{y}-{z}.png"), FileMode.OpenOrCreate))
-            data.SaveTo(stream);
+        {
+            picture.ToPng(100).SaveTo(stream);
+        }
     }
 
-    [Fact]
-    public async Task RenderTileTest2()
+    [Theory]
+    [InlineData(0, 0, 0)]
+    [InlineData(1, 0, 1)]
+    [InlineData(2, 1, 2)]
+    [InlineData(33, 22, 6)]
+    [InlineData(1072, 717, 11)]
+    [InlineData(8580, 5738, 14)]
+    [InlineData(8580, 5733, 14)]
+    [InlineData(8581, 5733, 14)]
+    public async Task RenderTileSvgTest(int x, int y, int z)
     {
-        var data = await _renderer.Render(new NetTopologySuite.IO.VectorTiles.Tiles.Tile(0, 0, 0));
+        var picture = await _renderer.Render(new NetTopologySuite.IO.VectorTiles.Tiles.Tile(x, y, z));
 
-        Assert.True(data != null);
-        Assert.True(!data.IsEmpty);
-        //Assert.True(data.Size == 126561);
+        Assert.True(picture != null);
 
-        using (var stream = new FileStream(Path.Combine(_path, "0-0-0.png"), FileMode.OpenOrCreate))
-            data.SaveTo(stream);
+        using (var stream = new FileStream(Path.Combine(_path, $"{x}-{y}-{z}.svg"), FileMode.OpenOrCreate))
+        {
+            picture.ToSvg(stream);
+        }
     }
-
 }
