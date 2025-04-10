61812315a2af7bb9f42d6fb5d2283a0fbdcec8b7
Charlenni <web@weltz-online.de>
2025-04-10 14:16:09 +0200

    or SKPictureAdd extens

---
+++ source/renderers/VexTile.Renderer.Common/Extensions/SKPictureExtensions.cs
@@ -0,0 +1,36 @@
+﻿using SkiaSharp;
+
+namespace VexTile.Renderer.Common.Extensions
+{
+    public static class SKPictureExtensions
+    {
+        public static SKData ToPng(this SKPicture picture, int quality = 100)
+        {
+            var bitmap = new SKBitmap((int)picture.CullRect.Width, (int)picture.CullRect.Height, SKColorType.Rgba8888, SKAlphaType.Opaque);
+            var canvas = new SKCanvas(bitmap);
+
+            canvas.DrawPicture(picture);
+
+            return bitmap.Encode(SKEncodedImageFormat.Png, quality);
+        }
+
+        public static SKData ToJpg(this SKPicture picture, int quality = 100)
+        {
+            var bitmap = new SKBitmap((int)picture.CullRect.Width, (int)picture.CullRect.Height, SKColorType.Rgba8888, SKAlphaType.Opaque);
+            var canvas = new SKCanvas(bitmap);
+
+            canvas.DrawPicture(picture);
+
+            return bitmap.Encode(SKEncodedImageFormat.Jpeg, quality);
+        }
+
+        public static void ToSvg(this SKPicture picture, Stream stream)
+        {
+            var canvas = SKSvgCanvas.Create(picture.CullRect, stream);
+
+            canvas.DrawPicture(picture);
+
+            canvas.Dispose();
+        }
+    }
+}
