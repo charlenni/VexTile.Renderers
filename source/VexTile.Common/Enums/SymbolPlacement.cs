e963da17581534ea4cdbcde0d92e2ea185d64292
Charlenni <web@weltz-online.de>
2025-04-16 13:26:57 +0200

    Add enum for symbol placement

---
+++ source/VexTile.Common/Enums/SymbolPlacement.cs
@@ -0,0 +1,17 @@
+﻿namespace VexTile.Common.Enums;
+
+public enum SymbolPlacement
+{
+    /// <summary>
+    /// Symbol is placed at the point where the geometry is located
+    /// </summary>
+    Point,
+    /// <summary>
+    /// Symbol is placed at the geometry line
+    /// </summary>
+    Line,
+    /// <summary>
+    /// Symbol is placed at center of line of geometry
+    /// </summary>
+    LineCenter
+}
