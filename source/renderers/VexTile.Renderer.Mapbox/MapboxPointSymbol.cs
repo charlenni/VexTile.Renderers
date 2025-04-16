d96790266953663507cc6d07df19e994779f4c47
Charlenni <web@weltz-online.de>
2025-04-16 13:26:36 +0200

    Add dummy implementation for Mapbox point symbol

---
+++ source/renderers/VexTile.Renderer.Mapbox/MapboxPointSymbol.cs
@@ -0,0 +1,13 @@
+﻿using VexTile.Renderer.Common.Interfaces;
+
+namespace VexTile.Renderer.Mapbox;
+
+public class MapboxPointSymbol : ISymbol
+{
+    public MapboxPointSymbol(ulong tileId)
+    {
+        TileId = tileId;
+    }
+
+    public ulong TileId { get; private set; }
+}
