34fca02ec0f387d252b4fc62f54d3a96c0c5f1d0
Charlenni <web@weltz-online.de>
2025-04-16 22:17:03 +0200

    Add a basic symbol class

---
+++ source/renderers/VexTile.Renderer.Mapbox/MapboxSymbol.cs
@@ -0,0 +1,52 @@
+﻿using NetTopologySuite.IO.VectorTiles.Tiles;
+using VexTile.Common.Primitives;
+using VexTile.Renderer.Common.Interfaces;
+using VexTile.Style.Mapbox;
+using VexTile.Style.Mapbox.Enums;
+
+namespace VexTile.Renderer.Mapbox;
+
+public class MapboxSymbol : ISymbol
+{
+    public MapboxSymbol(Tile tile)
+    {
+        Tile = tile;
+    }
+
+    /// <summary>
+    /// Tile Id to which this symbol belongs
+    /// </summary>
+    public Tile Tile { get; }
+
+    public double SortOrder { get; internal set; }
+
+    internal void CreateCommon(MapboxTileStyle style, EvaluationContext context)
+    {
+        switch (style.Layout.SymbolZOrder)
+        {
+            case SymbolZOrder.Source:
+                SortOrder = (double)(style.Layout.SymbolSortKey?.Evaluate(context) ?? 0.0);
+                break;
+            case SymbolZOrder.ViewportY:
+                if (style.Layout.IconAllowOverlap || style.Layout.TextAllowOverlap || style.Layout.IconIgnorePlacement || style.Layout.TextIgnorePlacement)
+                {
+                    SortOrder = Tile.Y * 512.0 + context.Feature.Geometry.Centroid.Y;
+                }
+                break;
+            case SymbolZOrder.Auto:
+                if (style.Layout.SymbolSortKey != null)
+                {
+                    SortOrder = (double)(style.Layout.SymbolSortKey?.Evaluate(context) ?? 0.0);
+                }
+                else if (style.Layout.IconAllowOverlap || style.Layout.TextAllowOverlap || !style.Layout.IconIgnorePlacement || !style.Layout.TextIgnorePlacement)
+                {
+                    SortOrder = Tile.Y * 512.0 + context.Feature.Geometry.Centroid.Y;
+                }
+                else
+                {
+                    SortOrder = 0;
+                }
+                break;
+        }
+    }
+}
