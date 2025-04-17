5e0b2cb81d570c361bf827becf661e7bc0d8652c
Charlenni <web@weltz-online.de>
2025-04-17 13:35:55 +0200

    Add exception for not founding icon in sprites

---
+++ source/renderers/VexTile.Renderer.Mapbox/Exceptions/SpriteNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace VexTile.Renderer.Mapbox.Exceptions;
+
+public class SpriteNotFoundException : Exception
+{
+    public SpriteNotFoundException(string message) : base(message)
+    {
+    }
+}
