057409c24af4474a6fbd4f768388a4dc3a5ca172
Charlenni <web@weltz-online.de>
2025-04-21 08:34:28 +0200

    Update test to RenderedTile

--- tests/VexTile.Styles.MapboxTests/Mapbox.Render.Tests.cs
+++ tests/VexTile.Styles.MapboxTests/Mapbox.Render.Tests.cs
@@ -1,4 +1,6 @@
-﻿using VexTile.Common.Interfaces;
+﻿using SkiaSharp;
+using VexTile.Common.Interfaces;
+using VexTile.Common.Primitives;
 using VexTile.Renderer.Common.Extensions;
 using VexTile.Renderer.Mapbox;
 using VexTile.Renderer.Picture;
@@ -10,7 +12,7 @@ namespace VexTile.Styles.MapboxTests;
 public class MapboxRenderTests
 {
     readonly string _path = "files";
-    PictureRenderer _renderer;
+    Renderer.Picture.Renderer _renderer;
 
     public MapboxRenderTests()
     {
@@ -18,7 +20,7 @@ public class MapboxRenderTests
 
         var mapboxStyleFile = MapboxStyleFileLoader.Load(stream).Result;
         
-        _renderer = new PictureRenderer(mapboxStyleFile.Sources.Select(s => (ITileSource)s.Value), mapboxStyleFile.Layers, new MapboxPaintFactory(mapboxStyleFile.Sprites), new MapboxSymbolFactory(mapboxStyleFile.Sprites));
+        _renderer = new Renderer.Picture.Renderer(mapboxStyleFile.Sources.Select(s => (ITileSource)s.Value), mapboxStyleFile.Layers, new MapboxPaintFactory(mapboxStyleFile.Sprites), new MapboxSymbolFactory(mapboxStyleFile.Sprites));
     }
 
     [Theory]
@@ -32,12 +34,19 @@ public class MapboxRenderTests
     [InlineData(8581, 5733, 14)]
     public async Task RenderTilePngTest(int x, int y, int z)
     {
-        (var picture, var symbols) = await _renderer.Render(new NetTopologySuite.IO.VectorTiles.Tiles.Tile(x, y, z));
+        var renderedTile = (RenderedTile) await _renderer.Render(new NetTopologySuite.IO.VectorTiles.Tiles.Tile(x, y, z));
 
-        Assert.True(picture != null);
+        Assert.True(renderedTile != null);
 
         using (var stream = new FileStream(Path.Combine(_path, $"{x}-{y}-{z}.png"), FileMode.OpenOrCreate))
         {
+            var pictureRecorder = new SKPictureRecorder();
+            var canvas = pictureRecorder.BeginRecording(new SKRect(0, 0, 512, 512));
+            var context = new EvaluationContext(z);
+
+            renderedTile.Draw(canvas, context);
+
+            var picture = pictureRecorder.EndRecording();
             picture.ToPng(100).SaveTo(stream);
         }
     }
@@ -53,12 +62,19 @@ public class MapboxRenderTests
     [InlineData(8581, 5733, 14)]
     public async Task RenderTileSvgTest(int x, int y, int z)
     {
-        (var picture, var symbols) = await _renderer.Render(new NetTopologySuite.IO.VectorTiles.Tiles.Tile(x, y, z));
+        var renderedTile = (RenderedTile) await _renderer.Render(new NetTopologySuite.IO.VectorTiles.Tiles.Tile(x, y, z));
 
-        Assert.True(picture != null);
+        Assert.True(renderedTile != null);
 
         using (var stream = new FileStream(Path.Combine(_path, $"{x}-{y}-{z}.svg"), FileMode.OpenOrCreate))
         {
+            var pictureRecorder = new SKPictureRecorder();
+            var canvas = pictureRecorder.BeginRecording(new SKRect(0, 0, 512, 512));
+            var context = new EvaluationContext(z);
+
+            renderedTile.Draw(canvas, context);
+
+            var picture = pictureRecorder.EndRecording();
             picture.ToSvg(stream);
         }
     }
