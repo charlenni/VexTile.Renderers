8e87bfa7a2523f89365eec3234ee01d8f02cda52
Charlenni <web@weltz-online.de>
2025-04-23 18:07:07 +0200

    Add a new project for tile data sources

---
+++ source/datasources/VexTile.TileDataSource/DefaultTileDataSourceFactory.cs
@@ -0,0 +1,40 @@
+﻿using VexTile.Common.Interfaces;
+
+namespace VexTile.TileDataSource;
+
+public static class DefaultTileDataSourceFactory
+{
+    public const string FileScheme = "file";
+    public const string MbTilesScheme = "mbtiles";
+    public const string HttpScheme = "http";
+    public const string HttpsScheme = "https";
+
+    public static ITileDataSource? CreateTileDataSource(string[] sources)
+    {
+        if (sources == null || sources.Length == 0)
+            return null;
+
+        // Allways use the first one, even if there are more than one
+        var source = sources[0];
+
+        // Uri has a limitation of ~2000 bytes for URLs, so extract the scheme by hand
+        var scheme = source.Substring(0, source.IndexOf(':'));
+
+        return scheme switch
+        {
+            MbTilesScheme => CreateMbTilesSource(source.Substring(MbTilesScheme.Length + 3)),
+            HttpScheme or HttpsScheme => CreateHttpSource(sources),
+            _ => throw new ArgumentException($"Scheme '{scheme}' of '{source}' is not supported"),
+        };
+    }
+
+    private static ITileDataSource CreateMbTilesSource(string source)
+    {
+        return new MbTilesTileDataSource.MbTilesTileDataSource(source);
+    }
+
+    private static ITileDataSource CreateHttpSource(string[] sources)
+    {
+        return new HttpTileDataSource.HttpTileDataSource(sources, 0, 6);
+    }
+}
---
+++ source/datasources/VexTile.TileDataSource/HttpTileDataSource/HttpTileDataSource.cs
@@ -0,0 +1,28 @@
+﻿using BruTile.Predefined;
+using BruTile.Web;
+using NetTopologySuite.IO.VectorTiles.Tiles;
+using VexTile.Common.Interfaces;
+
+namespace VexTile.TileDataSource.HttpTileDataSource
+{
+    public class HttpTileDataSource : ITileDataSource
+    {
+        HttpTileSource _httpTileSource;
+        HttpClientHandler _httpHandler;
+        HttpClient _httpClient;
+
+
+        public HttpTileDataSource(string[] sources, int minZoom, int maxZoom) 
+        {
+            _httpTileSource = new HttpTileSource(new GlobalSphericalMercator(minZoom, maxZoom), sources[0]);
+            _httpHandler = new HttpClientHandler { AllowAutoRedirect = true };
+            _httpClient = new HttpClient(_httpHandler);
+            _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", $"user-agent-of-{Path.GetFileNameWithoutExtension(System.AppDomain.CurrentDomain.FriendlyName)}");
+        }
+
+        public Task<byte[]?> GetTileAsync(Tile tile)
+        {
+            return _httpTileSource.GetTileAsync(_httpClient, new BruTile.TileInfo { Index = new BruTile.TileIndex(tile.X, tile.Y, tile.Zoom) });
+        }
+    }
+}
