87d035aa76e6b3d6470e4a18ffed473ccf34a75a
Charlenni <web@weltz-online.de>
2025-04-24 15:13:30 +0200

    Change text size to 20

--- source/renderers/VexTile.Renderer.Common/TileInformation.cs
+++ source/renderers/VexTile.Renderer.Common/TileInformation.cs
@@ -5,8 +5,12 @@ namespace VexTile.Renderer.Common;
 public record TileInformation
 {
     public bool Border { get; init; } = false;
+
     public bool Text { get; init; } = false;
+
     public float BorderSize { get; init; } = 2;
-    public float TextSize { get; init; } = 16;
+
+    public float TextSize { get; init; } = 20;
+
     public SKColor Color { get; init; } = SKColors.Red;
 }
