7763c7bd915a61784773f0ca0e77b77abbe97385
Charlenni <web@weltz-online.de>
2025-04-24 11:03:53 +0200

    Add class for tile information

---
+++ source/renderers/VexTile.Renderer.Common/TileInformation.cs
@@ -0,0 +1,12 @@
+﻿using SkiaSharp;
+
+namespace VexTile.Renderer.Common;
+
+public record TileInformation
+{
+    public bool Border { get; init; } = false;
+    public bool Text { get; init; } = false;
+    public float BorderSize { get; init; } = 2;
+    public float TextSize { get; init; } = 16;
+    public SKColor Color { get; init; } = SKColors.Red;
+}
